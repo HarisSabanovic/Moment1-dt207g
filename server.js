d93bd97d4ffbe985f0f60a141d80b6b3aed89ed5
Haris Sabanovic <hasa2303@student.miun.se>
2024-03-20 16:01:22 +0100

    lade till body-parser

--- server.js
+++ server.js
@@ -2,10 +2,12 @@
 //skapar webbserver
 const express = require("express");
 const app = express();
+const bodyparser = require("body-parser"); //kunna ta emot formulär
 const port = 3000;
 
 app.set("view engine", "ejs") //view engine EJS
 app.use(express.static("static-map"));  //statiska filer i katalogen static-map
+app.use(bodyparser.urlencoded({extended: true}));
 
 //routing
 app.get("/", (req, res) => {
