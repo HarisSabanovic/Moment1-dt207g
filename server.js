9a66c53070cae0e97ab5ca3c9579a17fe247a240
Haris Sabanovic <hasa2303@student.miun.se>
2024-03-20 12:59:25 +0100

    lade till nya vyer, sidor

--- server.js
+++ server.js
@@ -4,12 +4,23 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+app.set("view engine", "ejs") //view engine EJS
+app.use(express.static("static-map"));  //statiska filer i katalogen static-map
+
 //routing
 app.get("/", (req, res) => {
-    res.send("hello world")
+    res.render("index");
+});
+
+app.get("/addcourse", (req, res) => {
+    res.render("addcourse");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
 });
 
 //starta
 app.listen(port, () => {
-    console.log("server started on port" + port)
+    console.log("server started on port " + port)
 });
