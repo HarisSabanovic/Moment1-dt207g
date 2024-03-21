e0c6d9d1a267fdff234b3a210d588a2683a52d2b
Haris Sabanovic <hasa2303@student.miun.se>
2024-03-21 13:17:41 +0100

    hämtar databasen o dess värden

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 //skapar webbserver
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
 const bodyparser = require("body-parser"); //kunna ta emot formulär
 const port = 3000;
@@ -10,11 +11,35 @@ app.use(express.static("static-map"));  //statiska filer i katalogen static-map
 app.use(bodyparser.urlencoded({extended: true}));
 
 //skapar connection till mysql
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "course_db",
+    password: "harissparris",
+    database: "course_db"
+});
+
+connection.connect((error) => {
+    if(error) {
+        console.error("connection failed " + error);
+        return;
+    }
+
+    console.log("connected to MySQL");
+})
+
+
 
 
 //routing
-app.get("/", (req, res) => {
+app.get("/", (req, res) => {  
     res.render("index");
+    connection.query("SELECT * FROM courses", function (err, course) {
+        if (err) {
+            console.error("Error executing query:", err);
+            return;
+        }
+        console.log(course);
+    });
 });
 
 app.get("/addcourse", (req, res) => {
